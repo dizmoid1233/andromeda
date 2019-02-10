58270d8e31ae7258bc0f10a8fd68998e87a1c0b2
Benjamin Moss <benjamin@geminicloud.co.uk>
2019-02-10 17:10:31 +0000

    added login.php script

---
+++ php/login.php
@@ -0,0 +1,24 @@
+<?php 
+    session_start();
+    $username = $_POST['uname'];
+    $password = md5($_POST['pword']);
+    $mysql_db_hostname = "localhost";
+    $mysql_db_user = "andromeda";
+    $mysql_db_password = "Forsaken2019!";
+    $mysql_db_database = "andromeda";
+    $con = mysql_connect($mysql_db_hostname, $mysql_db_user, $mysql_db_password)
+    or die("Could not connect database");
+    mysql_select_db($mysql_db_database, $con)or die("Could not select database");
+
+    $query = "SELECT * FROM registered_users WHERE name='$username' AND password='$password'";
+    $result = mysql_query($query)or die(mysql_error());
+    $num_row = mysql_num_rows($result);
+    $row=mysql_fetch_array($result);
+    if( $num_row >=1 ) { 
+    echo 'true';
+    $_SESSION['user_name']=$row['uname'];
+    }
+    else{
+    echo 'false';
+    }
+?>
