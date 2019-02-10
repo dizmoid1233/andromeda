96251f30fbd5a00daa1eb060618915aef819be84
Benjamin Moss <benjamin@geminicloud.co.uk>
2019-02-10 20:37:22 +0000

    move to mysqli

--- index.php
+++ index.php
@@ -26,14 +26,14 @@ session_start();
         $mysql_db_user = "andromeda";
         $mysql_db_password = "Forsaken2019!";
         $mysql_db_database = "andromeda";
-        $con = mysql_connect($mysql_db_hostname, $mysql_db_user, $mysql_db_password)
+        $con = mysqli_connect($mysql_db_hostname, $mysql_db_user, $mysql_db_password)
         or die("Could not connect database");
-        mysql_select_db($mysql_db_database, $con)or die("Could not select database");
+        mysqli_select_db($mysql_db_database, $con)or die("Could not select database");
 
         $query = "SELECT * FROM registered_users WHERE name='$username' AND password='$password'";
-        $result = mysql_query($query)or die(mysql_error());
-        $num_row = mysql_num_rows($result);
-        $row=mysql_fetch_array($result);
+        $result = mysqli_query($query)or die(mysqli_error());
+        $num_row = mysqli_num_rows($result);
+        $row=mysqli_fetch_array($result);
         if( $num_row >=1 ) { 
         echo 'true';
         $_SESSION['user_name']=$row['uname'];
