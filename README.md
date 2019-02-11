01f26d86b5ecf544f796c35df64cc0a0fb32243c
Benjamin Moss <benjamin@geminicloud.co.uk>
2019-02-11 21:22:53 +0000

    attempt some php stuff on client landing

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 # Andromeda
 
 Andromeda is a web-based tracking tool for device repairs. I've written it to help me manage the devices I repair, but I thought others might find the concept useful too so I've made it public. Hopefully it becomes what I am envisioning, but as I'm new to programming we'll have to see.
+
+## To Do:
+
+ - Implement admin interface
+ - Implement client interface
---
+++ sub/client_landing.php
@@ -0,0 +1,32 @@
+<?php
+    $username = $_SESSION['username'];
+    $mysqli_db_hostname = "localhost";
+    $mysqli_db_user = "andromeda";
+    $mysqli_db_password = "Forsaken2019!";
+    $mysqli_db_database = "andromeda";
+    $con = mysqli_connect($mysqli_db_hostname, $mysqli_db_user, $mysqli_db_password, $mysqli_db_database)
+    or die("Could not connect database");
+    mysqli_select_db($con, $mysqli_db_database)or die("Could not select database");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Your Device</title>
+        <div class="header">
+            <?php
+                $query = mysqli_query($con, "SELECT device_owner FROM devices WHERE username='$username'")
+                $row = $query->fetch_assoc();
+            ?>
+            <p class="title"><?php echo $row['device_owner'];?></p>
+            <div class="button">
+                <p class="button"><a class="header">Log out</a></p>
+            </div>
+        </div>
+        <link href="../css/main.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="card">
+            <p>Client landing page</p>
+        </div>
+    </body>
+</html>
