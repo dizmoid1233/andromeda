5da392c21c4fcbe77acf6860b69d52e38dc0bd28
Benjamin Moss <benjamin@geminicloud.co.uk>
2019-02-11 11:04:09 +0000

    test redirect using log out button

---
+++ php/redirect.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    $mysqli_db_hostname = "localhost";
+    $mysqli_db_user = "andromeda";
+    $mysqli_db_password = "Forsaken2019!";
+    $mysqli_db_database = "andromeda";
+    $con = mysqli_connect($mysqli_db_hostname, $mysqli_db_user, $mysqli_db_password, $mysqli_db_database)
+    or die("Could not connect database");
+    mysqli_select_db($con, $mysqli_db_database)or die("Could not select database");
+
+    $query = "SELECT admin FROM users WHERE username='$username' AND password='$password'";
+    print_r($query)
+?>
