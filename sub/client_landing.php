83af72f3d5fa15f3b49ccbda6d81f7d7ab80c1ef
Benjamin Moss <benjamin@geminicloud.co.uk>
2019-02-14 09:01:04 +0000

    move to pdo and add test query

---
+++ sub/client_landing.php
@@ -0,0 +1,48 @@
+<?php
+    $servername = "localhost";
+    $sql_user = "andromeda";
+    $password = "Forsaken2019!";
+    
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=andromeda", $sql_user, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+        }
+    catch(PDOException $e)
+        {
+        echo "Connection failed: " . $e->getMessage();
+        }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Your Device</title>
+        <div class="header">
+            <p class="title">yote</p>
+            <div class="button">
+                <p class="button"><a class="header">Log out</a></p>
+            </div>
+        </div>
+        <link href="../css/main.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <?php
+            $username = $_COOKIE('usrinfo');
+            $sql = "SELECT * FROM users where username=$username";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                   echo $row['device_owner'];
+                }
+            } else {
+                echo "0 results";
+            }
+            $conn->close();
+        ?>
+        <div class="card">
+            <p>this is a test of my nan</p>
+            <p><?php echo $row['device_owner']?></p>
+        </div>
+    </body>
+</html>
